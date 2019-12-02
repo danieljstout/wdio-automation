146f43c2f87ed264c4616dba974dbea36620fab3
Daniel Stout <dstout@mdlive.com>
2019-12-02 13:48:55 -0500

    initial commit

---
+++ test/specs/something.spec.js
@@ -0,0 +1,7 @@
+describe("go to google", () => {
+  it("should reach an error page using an ineligible patient", () => {
+    console.log("\n\n\n\n\n\n\n\n\n\n\n\n hello!!!! \n\n\n\n\n\n\n\n\n\n\n\n\n")
+    browser.url("https://google.com/");
+    browser.debug();
+  });
+});
